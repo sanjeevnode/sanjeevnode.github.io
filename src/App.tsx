33d8c64ce76275abb5a735d9114bc7755d39bc00
sanjeevnode <me.sanjeevks@gmail.com>
2025-01-07 17:06:38 +0530

    initila

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <main className="h-screen w-screen bg-white flex justify-center items-center">
+      <p className="text-[5vh] text-gray-500 p-10">
+        Hi, I am <span className="text-gray-900 font-semibold">Sanjeev Kumar Singh</span> and I am a Full Stack Developer.
+      </p>
+    </main>
+  )
+}
+
+export default App
+
