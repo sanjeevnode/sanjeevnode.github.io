07940b0280424adf6fb96c7820949c4581d9b98b
sanjeevnode <me.sanjeevks@gmail.com>
2025-05-18 11:24:11 +0530

    refactor: restructure admin login page and move AdminLogin component to its own file

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,31 +1,10 @@
-'use client';
+import AdminLogin from '@/components/Login';
+import { Suspense } from 'react';
 
-import { signIn, useSession } from 'next-auth/react';
-import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-export default function AdminLogin() {
-    const searchParams = useSearchParams();
-    const session = useSession();
-    const router = useRouter();
-    const callbackUrl = searchParams.get('callbackUrl') || '/admin/cms';
-
-    useEffect(() => {
-        if (session?.status === "authenticated") {
-            console.log("authenticated")
-            router.push("/admin/cms");
-        }
-    }, [session?.status, router])
-
+export default function Page() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <h2 className="text-xl mb-4">Admin Login</h2>
-            <button
-                className="px-4 py-2 bg-black text-white rounded"
-                onClick={() => signIn('github', { callbackUrl })}
-            >
-                Sign in with GitHub
-            </button>
-        </div>
+        <Suspense>
+            <AdminLogin />
+        </Suspense>
     );
 }
